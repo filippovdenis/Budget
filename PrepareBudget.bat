5f1789d38ce203fa4dc96a2b3219f1cfe667e723
Denis Filippov <dfilippov@oneincsystems.com>
2019-01-25 14:54:41 +0700

    Fix PrepareBudget

--- PrepareBudget.bat
+++ PrepareBudget.bat
@@ -1,6 +1,4 @@
 git tag --sort=-v:refname --merged > 1.txt
-Set /P MyVar=<1.txt
+Set /P VersionTag=<1.txt
 
 echo %VersionTag%
-
-"C:\Program Files\7-Zip\7z.exe" a Sequel.Budget.%MyVar%.%1.zip" "Budget.sln" -mx1 -mmt2
