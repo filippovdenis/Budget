dd0148b3def878b016af293bd418ff223a3bb848
Denis Filippov <dfilippov@oneincsystems.com>
2019-01-25 15:32:25 +0700

    Fix PrepareBudget.bat

--- PrepareBudget.bat
+++ PrepareBudget.bat
@@ -2,4 +2,4 @@ git tag --sort=-v:refname --merged > 1.txt
 Set /P VersionTag=<1.txt
 
 
-echo ##teamcity[setParameter name='env.VersionTag' value='%VerTag%']
+echo ##teamcity[setParameter name='env.VersionTag' value='%VersionTag%']
