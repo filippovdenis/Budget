d93246c187b524140a99fc0d6ef9535268cdd87e
Denis Filippov <dfilippov@oneincsystems.com>
2019-01-25 14:52:45 +0700

    Add version tag info

--- PrepareBudget.bat
+++ PrepareBudget.bat
@@ -1,5 +1,6 @@
-git tag --points-at > 1.txt
+git tag --sort=-v:refname --merged > 1.txt
 Set /P MyVar=<1.txt
 
+echo %VersionTag%
 
 "C:\Program Files\7-Zip\7z.exe" a Sequel.Budget.%MyVar%.%1.zip" "Budget.sln" -mx1 -mmt2
