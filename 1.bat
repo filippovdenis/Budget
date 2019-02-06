4e68382a4dc60c6cba66eb580bd0b3b16747993f
Denis Filippov <dfilippov@oneincsystems.com>
2019-02-06 11:11:25 +0700

    Add files deletion

--- 1.bat
+++ 1.bat
@@ -1,1 +1,3 @@
+del d:\Projects\policyone.dataone\OneInc.PolicyOne.DataOne.SSIS\OneInc.PolicyOne.DataOne.SSIS.Transform.Load.P1Data\*.dtsx
+
 "d:\MyProjects\VSOLE\ConsoleApp1\ConsoleApp1\bin\Debug\ConsoleApp1.exe" "OneInc.PolicyOne.DataOne.SSIS\OneInc.PolicyOne.DataOne.SSIS.Transform.Load.P1Data\Miscellaneous\OneInc.PolicyOne.DataOne.SSIS.Transform.Load.P1Data.biml"
